37afdb59a813ff647871cbf91893c584809ad4c8
lianaglennon <lianaglennon@users.noreply.github.com>
2021-03-09 23:29:48 -0500

    fixed js - added variables for all inputs and outputs; altered css- buttons and padding

--- app.js
+++ app.js
@@ -1,57 +1,68 @@
 // Inputs
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
+let street = document.getElementById('street');
+let apartment = document.getElementById('apartment');
+let city = document.getElementById('city');
+let state = document.getElementById('state');
+let zipcode = document.getElementById('zip');
+let salary = document.getElementById('salary');
+let phone = document.getElementById('phone');
+let date = document.getElementById('date');
 let relocate = document.getElementsByName(`relocate`);
 
 
 // Outputs
 let firstNameResponse = document.getElementById(`first-name--response`);
+let lastNameResponse = document.getElementById(`last-name--response`);
+let streetResponse = document.getElementById(`st-address--response`);
+let apartmentResponse = document.getElementById(`apartment--response`);
+let cityResponse = document.getElementById(`city--response`);
+let stateResponse = document.getElementById(`state--response`);
+let zipResponse = document.getElementById(`zip--response`);
+let salaryResponse = document.getElementById(`salary--response`);
+let phoneResponse = document.getElementById(`phone--response`);
+let dateResponse = document.getElementById(`date--response`);
+let relocateResponse = document.getElementById(`relocation--response`);
 
 
+firstName.addEventListener('keyup', (e) => {
+        firstNameResponse.textContent = `First Name: ${e.target.value}`;
+    }, false);
+lastName.addEventListener('keyup', (e) => {
+        lastNameResponse.textContent = `Last Name: ${e.target.value}`;
+    }, false);
+street.addEventListener('keyup', (e) => {
+        streetResponse.textContent = `Street Address: ${e.target.value}`;
+    }, false);
+apartment.addEventListener('keyup', (e) => {
+        apartmentResponse.textContent = `Apartment: ${e.target.value}`;
+    }, false);
+city.addEventListener('keyup', (e) => {
+        cityResponse.textContent = `City: ${e.target.value}`;
+    }, false);
+state.addEventListener('keyup', (e) => {
+        stateResponse.textContent = `State: ${e.target.value}`;
+    }, false);
+zipcode.addEventListener('keyup', (e) => {
+        zipResponse.textContent = `Zipcode: ${e.target.value}`;
+    }, false);
+salary.addEventListener('keyup', (e) => {
+        salaryResponse.textContent = `Salary: ${e.target.value}`;
+    }, false);
+phone.addEventListener('keyup', (e) => {
+        phoneResponse.textContent = `Phone Number: ${e.target.value}`;
+    }, false);
+date.addEventListener('change', (e) => {
+        dateResponse.textContent = `Date: ${e.target.value}`;
+    }, false);
 
 for( let i = 0; i < relocate.length; i++ ) {
     relocate[i].addEventListener(`click`, (e) => {
-        console.log(e.target.value);
-        firstNameResponse.textContent = `The response was ${e.target.value}`;
+        // console.log(e.target.value);
+        relocateResponse.textContent = `Relocate? ${e.target.value}`;
     }, false);
 }
 
 
 
-// function updateInput(evt) {
-//     let element = evt.target;
-//     let value = element.value;
-//     let outputId = element.getAttribute("data-output");
-//     let output = document.getElementById(outputId);
-//     output.value = value;
-// }
-//
-// function radioInput() {
-//     for (let i = 0; i < location.length; i++) {
-//         if (location[i].checked) {
-//             locationAnswer = location[i].value;
-//             let radioOutputId = getElementById("relocateAnswer");
-//             let radioOutput = document.getElementById(radioOutputId);
-//             radioOutput.value = locationValue;
-//         }
-//     }
-//     let radioOutputId = getElementById("relocateAnswer");
-//     let radioOutput = document.getElementById(radioOutputId);
-//     radioOutput.value = locationValue;
-// }
-
-// document.getElementById("firstName").addEventListener("keyup", updateInput);
-// document.getElementById("lastName").addEventListener("keyup", updateInput);
-// document.getElementById("street").addEventListener("keyup", updateInput);
-// document.getElementById("apartment").addEventListener("keyup", updateInput);
-// document.getElementById("city").addEventListener("keyup", updateInput);
-// document.getElementById("state").addEventListener("keyup", updateInput);
-// document.getElementById("zip").addEventListener("keyup", updateInput);
-// document.getElementById("salary").addEventListener("keyup", updateInput);
-// document.getElementById("number").addEventListener("keyup", updateInput);
-// document.getElementById("date").addEventListener("keyup", updateInput);
-
-//relocate.addEventListener(`click`, radioInput(), false);
-
-// relocate[0].addEventListener("click", radioInput);
-// relocate[1].addEventListener("click", radioInput);
-// relocate[2].addEventListener("click", radioInput);
-// relocate[3].addEventListener("click", radioInput);
